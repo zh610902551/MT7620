868c41d1adc9a65edb0e7567f39bfa33e41a8699
zh610902551 <610902551@qq.com>
2020-04-01 17:28:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone  https://github.com/Leo-Jo/luci-app-vssr package/luci-app-vssr
+git clone  https://github.com/Leo-Jo/luci-app-vssr
 CONFIG_PACKAGE_luci-app-vssr=y
